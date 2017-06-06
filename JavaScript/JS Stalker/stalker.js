beeb3022b749792bd59319cf458ab0b5645c71e7
Ankit Tiwari <ankitt255@gmail.com>
2017-06-06 12:10:14 +0530

    Add solution for javascript stalker exercise

---
+++ JavaScript/JS Stalker/stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
