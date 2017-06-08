0fc210800b118eeeb95bbff93a338b92b949d60e
Ankit Tiwari <ankitt255@gmail.com>
2017-06-08 15:17:10 +0530

    Update solution for JS Todo List excercise

--- JavaScript/Todo List/list.js
+++ JavaScript/Todo List/list.js
@@ -1,16 +1,39 @@
-var todo = [];
+var todos = [];
 
 var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
-	if(input === "list")
-		console.log(todo);
+	if(input === "list") {
+		listTodos();
+	}
 	else if (input === "new") {
-		var newTodo = prompt("Enter a new todo");
-		todo.push(newTodo);
+		addTodo();
+	}
+	else if(input === "delete") {
+		deleteTodo();
 	}
 
 	input = prompt("What would you like to do?");
 }
 
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+	console.log("********");
+	todos.forEach(function(todo,i) {
+		console.log(i + ": " + todo);
+	});
+	console.log("********");
+}
+
+function addTodo() {
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+function deleteTodo() {
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index,1);
+	console.log("Deleted Todo");
+}
