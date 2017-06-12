7c84fc53961f90c544a15203df293dfb625312bd
Ankit Tiwari <ankitt255@gmail.com>
2017-06-12 20:25:22 +0530

    Add solution for DOM selector excercise

---
+++ DOM/selector.js
@@ -0,0 +1,17 @@
+var p = document.getElementById("first");
+
+var p = document.querySelector("#first");
+
+var p = document.getElementsByClassName("special");
+
+var p = document.querySelector(".special");
+
+var p = document.querySelector("p");
+
+var p = document.querySelectorAll(".special")[0];
+
+var p = document.getElementByTagName("p")[0];
+
+var p = document.querySelectorAll("p")[0];
+
+var p = document.querySelector("h1 + p");
