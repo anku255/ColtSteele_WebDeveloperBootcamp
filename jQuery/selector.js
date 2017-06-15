3c009d6cca11e7936c6a439ca208a860422416de
Ankit Tiwari <ankitt255@gmail.com>
2017-06-15 10:49:34 +0530

    Add solution for jQuery selector exercise

---
+++ jQuery/selector.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("background","purple");
+
+// Select the divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width","200px");
+
+// Select the div with id "third" and give it a orange border
+$("#third").css("border", "2px solid orange");
+
+// Select the first div only and change its font color to pink
+$("div:first-of-type").css("color","pink");
