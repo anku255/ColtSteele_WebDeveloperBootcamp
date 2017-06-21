5da87c140627ae8bd883e33ed0e87380190fb09d
Ankit Tiwari <ankitt255@gmail.com>
2017-06-21 13:53:04 +0530

    Add a route to add a new campground in YelpCamp project

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,23 +1,42 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-
+var campgrounds = [
+		{name: "Salmon Creek", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+		{name: "Granite Hill", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2535/3823437635_c712decf64.jpg"}
+	]
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-	var campgrounds = [
-		{name: "Salmon Creek", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
-		{name: "Granite Hill", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-		{name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2535/3823437635_c712decf64.jpg"}
-	]
 
 	res.render("campgrounds", {campgrounds: campgrounds});
 
+});
+
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new")
+});
+
+app.post("/campgrounds", function(req, res) {
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {
+		name: name,
+		image: image
+	};
+	campgrounds.push(newCampground);
+	// redirect back to camgrounds page
+	res.redirect("/campgrounds");
 });
 
 app.listen(3000, function(req, res) {
