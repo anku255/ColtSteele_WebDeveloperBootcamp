1ac50347a2f46f15d93baecea048342dde3caac6
Ankit Tiwari <ankitt255@gmail.com>
2017-06-22 16:33:15 +0530

    Add Show route to show more details of a blog in RESTfulBlogApp

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -60,6 +60,18 @@ app.post("/blogs", function(req, res) {
 	
 });
 
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("show",{blog: foundBlog});
+		}
+	});
+});
+
 app.listen(3000, function() {
 	console.log("Server has started at PORT 3000");
 });
