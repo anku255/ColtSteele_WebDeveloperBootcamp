33b30da9533283ba7a7f11ba664eccba8f1b004d
Ankit Tiwari <ankitt255@gmail.com>
2017-06-22 16:00:14 +0530

    Add Create route to submit a new blog in RESTfulBlogApp

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -30,6 +30,7 @@ app.get("/", function(req, res) {
 	res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if(err) {
@@ -40,7 +41,24 @@ app.get("/blogs", function(req, res) {
 	});
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+	res.render("new");
+});
 
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+	// create blog
+	Blog.create(req.body.blog , function(err, newBlog) {
+		if(err) {
+			res.render("new");
+		} else {
+			// redirect to the index
+			res.redirect("/blogs");
+		}
+	}); 
+	
+});
 
 app.listen(3000, function() {
 	console.log("Server has started at PORT 3000");
