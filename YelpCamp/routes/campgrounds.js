1681ba3bbe16321ff2afaf1920c32978cd73b8af
Ankit Tiwari <ankitt255@gmail.com>
2017-07-02 16:21:00 +0530

    Add delete button to delete a campground in YelpCamp project

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -85,6 +85,17 @@ router.put("/:id", function(req, res) {
   });
 });
 
+// DESTROY Campground Route
+router.delete("/:id/", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if(err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // middleware function to check if user is logged in
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
