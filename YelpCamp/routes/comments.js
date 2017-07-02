54f9a4ded281ba3f0608aead79228c7fab2d919a
Ankit Tiwari <ankitt255@gmail.com>
2017-07-02 23:12:47 +0530

    Only authorized user can edit/delete a comment in YelpCamp Project

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // Show edit form for Comment
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if(err) {
       res.redirect("back");
@@ -56,7 +56,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // UPDATE comment
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
     if(err) {
       res.redirect("back");
@@ -67,7 +67,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // DESTROY comment route
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if(err) {
       res.redirect("back");
@@ -77,7 +77,27 @@ router.delete("/:comment_id", function(req, res) {
   });
 });
 
-// middleware function to check if user is logged in
+// middleware
+function checkCommentOwnership(req, res, next) {
+  if(req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if(err) {
+        res.redirect("back");
+      } else {
+        // does user own the comment?
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+// middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
