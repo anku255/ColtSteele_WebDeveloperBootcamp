e5db1ce7e25147b5ea831fc71f2d0a3bd7ef7918
Ankit Tiwari <ankitt255@gmail.com>
2017-07-02 15:04:41 +0530

    Associate a campground with user in YelpCamp project

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -5,12 +5,19 @@ var campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Comment"
     }
-  ]
+  ],
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   res.render("campgrounds/new")
 });
 
@@ -35,15 +35,20 @@ router.get("/:id", function(req, res) {
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
   var newCampground = {
     name: name,
     image: image,
-    description: desc
+    description: desc,
+    author: author
   };
   // Add newCampground to the database
   Campground.create(newCampground, function(err,newlyCreated) {
@@ -55,5 +60,13 @@ router.post("/", function(req, res) {
     }
   });
 });
+
+// middleware function to check if user is logged in
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
